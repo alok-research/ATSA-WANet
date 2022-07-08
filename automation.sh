00c1497847c68dedbe38b253508671b16f7c778c
Alok Singh <100084967+alok-research@users.noreply.github.com>
2022-07-09 03:41:25 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 working_on_user_program_file=edit_$user_program_name_cp
+#modify_tr_data_file_name=$tr_data_file_name-$name_according_x_axis-$k--$var_name-$l--run
 modify_tr_data_file_name=$var_name-$l--$name_according_x_axis-$k--run
+
 modify_metrics_data_file_name=$metrics_data_file_name-$name_according_x_axis-$k--$var_name-$l
 ref_value=$k
 
@@ -22,17 +24,15 @@ for ((iteration=1; iteration<=$run; iteration++))
 	do
 	
 		echo											
-		echo "*** Simulation is executing for run number : $iteration // $name_according_x_axis-$k // $var_name-$l ***"
-		echo
-		echo  `date +"Date %F and Execution Time start for this run (hh:mm:ss) %T"`
+		echo  "*** " `date +"Date %F and Time (hh:mm:ss) %T"`" - Simulation execution start for run number : $iteration // $name_according_x_axis-$k // $var_name-$l ***"
 		echo
 		cd $ns3_path/ns-allinone-$ns3_version/ns-$ns3_version/scratch
 		sed -e 's/SetRun(1)/SetRun('$iteration')/g' $working_on_user_program_file > $iteration.cc
 		cd ..
 		
 		#./waf --run scratch/$iteration
-		./ns3 run scratch/$iteration		
-		
+		./ns3 run scratch/$iteration
+				
 		rm scratch/$iteration.cc
 		mv $tr_data_file_name "$ns3_path"/$directory
 		cd ~
@@ -44,49 +44,27 @@ for ((iteration=1; iteration<=$run; iteration++))
 		sed -i 's/(/( /g' $modify_tr_data_file_name-$iteration--$tr_data_file_name
 		sed -i 's/=/ /g' $modify_tr_data_file_name-$iteration--$tr_data_file_name
 		sed -i 's/,//g' $modify_tr_data_file_name-$iteration--$tr_data_file_name
+		echo
+		echo "*** Now, start metrics data computation for trace file name '$modify_tr_data_file_name-$iteration--$tr_data_file_name' ***"
+		echo "#" `date` >>$modify_metrics_data_file_name
+		sed -e 's/run=1/run='$iteration'/g' metrics_computation1.awk >metrics_computation1_$iteration.awk
+		awk -f metrics_computation1_$iteration.awk $modify_tr_data_file_name-$iteration--$tr_data_file_name >>$modify_metrics_data_file_name
+		echo "*** metrics data computation has been done and data has been stored in a file- '$modify_metrics_data_file_name' ***"
+		echo
+		rm metrics_computation1_$iteration.awk
+		rm $modify_tr_data_file_name-$iteration--$tr_data_file_name
 		cd ~
-			
 	done
 
-cd ~ || exit
 cd "$ns3_path"/$directory || exit
 
-# ******Start *******for configure in gnu configure file
-#echo "count = $n_count"
-sed -i '/count=/d' gnu_configure.sh
-sed -i 's/.*plot.*/count='$n_count'\n&/' gnu_configure.sh
-sed -i '/j_value_start=/d' gnu_configure.sh
-sed -i 's/.*plot.*/j_value_start='$var_value_first'\n&/' gnu_configure.sh
-sed -i '/j_value_diff=/d' gnu_configure.sh
-sed -i 's/.*plot.*/j_value_diff='$var_value_diff'\n&/' gnu_configure.sh
-
-
-sed -i '/x_min=/d' gnu_configure.sh
-sed -i '/x_max=/d' gnu_configure.sh
-sed -i '/x_delta=/d' gnu_configure.sh
-sed -i 's/.*x_minimum.*/x_min='$x_min'\n&/' gnu_configure.sh
-sed -i 's/.*x_minimum.*/x_max='$x_max'\n&/' gnu_configure.sh
-sed -i 's/.*x_minimum.*/x_delta='$x_delta'\n&/' gnu_configure.sh
-#*********End**********
-
-echo "#" `date` >$metrics_data_file_name
-	for ((b=1; b<=$run; b++))					
-	do
-		sed -e 's/run=1/run='$b'/g' metrics_computation1.awk >metrics_computation1_$b.awk
-		awk -f metrics_computation1_$b.awk $modify_tr_data_file_name-$b--$tr_data_file_name >>$metrics_data_file_name
-		rm metrics_computation1_$b.awk
-		rm $modify_tr_data_file_name-$b--$tr_data_file_name
-	done
-mv $metrics_data_file_name $modify_metrics_data_file_name
 #sed -i 's/-nan/'0'/g' $modify_metrics_data_file_name
-
+echo
+echo "*** Now, start statistics computation of data points = $run ***"
 sed -i '/^rvalue/c rvalue='$ref_value'' statistics_computation.awk
 awk -f statistics_computation.awk $modify_metrics_data_file_name >>$modify_statistics_data_file_name.csv
 awk -f statistics_computation.awk $modify_metrics_data_file_name >>$modify_statistics_data_file_name
-
-#echo
+echo "*** statistics computation has been done and data has been stored in a file- '$modify_statistics_data_file_name' ***"
+echo
 cd ~
 rm "$ns3_path"/ns-allinone-$ns3_version/ns-$ns3_version/scratch/$working_on_user_program_file
-echo 'Done!'
-#nautilus $directory
-
